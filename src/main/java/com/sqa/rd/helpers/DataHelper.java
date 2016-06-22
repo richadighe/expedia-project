080f81c9c093345093351cededb8cab69b0beff5
Richa Dighe <richadighe@gmail.com>
2016-06-21 21:52:01 -0700

    delete the data helper test class

--- src/main/java/com/sqa/rd/helpers/DataHelper.java
+++ src/main/java/com/sqa/rd/helpers/DataHelper.java
@@ -11,12 +11,78 @@
 package com.sqa.rd.helpers;
 
 import java.io.*;
+import java.sql.*;
 import java.util.*;
 import java.util.regex.*;
 
 import com.sqa.rd.helpers.data.*;
 
 public class DataHelper {
+
+	public static Object[][] evalDatabaseTable(String driverClassString, String databaseStringUrl, String username,
+			String password, String tableName) throws ClassNotFoundException, SQLException {
+
+		return evalDatabaseTable(driverClassString, databaseStringUrl, username, password, tableName, 0, 0);
+	}
+
+	/**
+	 * @param driverClassString
+	 * @param databaseStringUrl
+	 * @param username
+	 * @param password
+	 * @param tableName
+	 * @param rowOffset
+	 * @param colOffset
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public static Object[][] evalDatabaseTable(String driverClassString, String databaseStringUrl, String username,
+			String password, String tableName, int rowOffset, int colOffset)
+			throws ClassNotFoundException, SQLException {
+
+		Object[][] myData;
+		ArrayList<Object> myArrayData = new ArrayList<Object>();
+
+		Class.forName(driverClassString);
+
+		Connection dbconn = DriverManager.getConnection(databaseStringUrl, username, password);
+
+		Statement stmt = dbconn.createStatement();
+
+		ResultSet rs = stmt.executeQuery("select * from " + tableName);
+
+		int numOfColumns = rs.getMetaData().getColumnCount();
+		int curRow = 1;
+
+		while (rs.next()) {
+			if (curRow > rowOffset) {
+				Object[] rowData = new Object[numOfColumns - colOffset];
+
+				for (int i = 0, j = colOffset; i < rowData.length; i++) {
+					rowData[i] = rs.getString(i + colOffset + 1);
+				}
+
+				myArrayData.add(rowData);
+
+			}
+			curRow++;
+
+		}
+
+		myData = new Object[myArrayData.size()][];
+
+		for (int i = 0; i < myData.length; i++) {
+			myData[i] = (Object[]) myArrayData.get(i);
+		}
+
+		// Step 5
+		rs.close();
+		stmt.close();
+		dbconn.close();
+
+		return myData;
+	}
 
 	public static Object[][] getTextFileData(String fileLocation, String fileName, TextFormat textFormat) {
 		return getTextFileData(fileLocation, fileName, textFormat, false);
