aa5e24efbc29c338a74d3c9e155b3d76e1dbc94b
Richa Dighe <richadighe@gmail.com>
2016-06-23 15:06:03 -0700

    add the selenium test web page initialization test class

---
+++ src/test/java/com/sqa/rd/helpers/WebPageInit.java
@@ -0,0 +1,62 @@
+package com.sqa.rd.helpers;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import org.apache.commons.io.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.*;
+
+public class WebPageInit {
+	private static String baseURL = "http://www.google.com";
+	private static WebDriver driver;
+
+	public static void captureScreenshot(String keywords) {
+		// get screenshots of test
+		File src = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(src, new File("screenshots/" + keywords + "_" + new Date().getTime() + ".png"));
+		} catch (IOException e) {
+		}
+	}
+	// @DataProvider
+	// public Object[][] dp() {
+	// return new Object[][] { new Object[] { 1, "a" }, new Object[] { 2, "b" },
+	// };
+	// }
+
+	/**
+	 * @return the baseURL
+	 */
+	public static String getBaseURL() {
+		return baseURL;
+	}
+
+	/**
+	 * @return the driver
+	 */
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	@BeforeClass
+	public static void setup() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(baseURL);
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		driver.quit();
+	}
+
+	// constructor to initialize base URL
+	public WebPageInit(String baseURL) {
+		super();
+		WebPageInit.baseURL = baseURL;
+	}
+
+}
